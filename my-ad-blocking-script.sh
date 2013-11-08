86b979a41e40bc2897506bf3850855cc104110e0
SilentBob999 <mitchou_114@hotmail.com>
2013-11-08 12:54:46 -0500

    HUGE COMMIT (made from github.com directly), to be split on different commit later
    HUGE COMMIT (made from github.com directly), to be split on different commit later
    Not tested on a router yet
    
    Put all file/filepath between quote... cifs1 path can contain space
    
    Optimize the removal of localhost entry from source files.
    
    Optimize the wget (try at first with default user string and if it fail it try another time with IE user-string)
    
    Add The possibility to choose between dnsmasq config file and host format (per source)
    	# Source that are in GETS but are not listed in KeepAsHost will be formated an use directly in the dnsmasq config. Like "address=/blockeddomain/0.0.0.0"
    	# Source in KeepAsHost="" (also need to be in GETS) will be merge in one HOST file and add to the dnsmasq config like this "addn-hosts=/cifs1/dnsmasq/genHost"
    Please note that duplicate entry between both type are not removed (duplicate inside each of them are still removed however)
    	# DNSmasq block domain wide, so every www. entry are removed and duplicate cleaned give smaller file.		
    	# Entry for localhost are added to the host file so it can be use to merge all source and create a good host file for a pc.
    	# Host file seems to use less RAM or at least it is more tolerant to start the service but may crash after a while. (the dnsmasq can crash outside the script perimeter)
    	I recommend dnsmasq "config only" because I can handle the result of the service start and fail safe to a not blockng but working state..
    
    ExtrA backup of file can be found in CIFS1	
    [ -d "$CIFS" -a -f "$GEN" ] && cp -f "$GEN" "$CIFS/dnsmask.conf"
    [ -d "$CIFS" -a -f "$GENHOST" ] && cp -f "$GENHOST" "$CIFS/addHost.conf"

--- my-ad-blocking-script.sh
+++ my-ad-blocking-script.sh
@@ -2,7 +2,6 @@
 ## Tomato AD-Blocking script
 ## https://github.com/SilentBob999/adblock
 
-
 REDIRECTIP="0.0.0.0"
 CIFS="/cifs1/dnsmasq" # adapt to your need
 CIFSRequire="N" # "Y"/"N"
@@ -11,7 +10,14 @@ WHITELIST="facebook.com dropbox.com"
 
 ## Sources
 ## Warning ( ( HUGE : S5 , S8 ) ( BIG : S6 ) CIFSRequire to "Y"
-GETS="1 2 3 4 7"
+GETS="1 2 3 4 7" # List all source you want here
+KeepAsHost="" # Listed here will be merge together in 1 single host (used as "addn-hosts=") 
+
+# Source that are in GETS but are not listed in KeepAsHost will be formated an use directly in the dnsmasq config.
+# Duplicate are removed inside each file but not duplicate between them.
+# DNSmasq block domain wide, so every www. entry are removed and duplicate cleaned give smaller file.
+# Host file seems to use less RAM or at least it is more tolerant to start the service but may crash after a while.
+
 S1="http://pgl.yoyo.org/as/serverlist.php?hostformat=nohtml"  ##44K - 2,539 hosts
 S2="http://mirror1.malwaredomains.com/files/justdomains" ##474K - 23,972 hosts
 S3="http://www.malwaredomainlist.com/hostslist/hosts.txt" ##52K - 1,661 hosts
@@ -25,10 +31,11 @@ S8="http://adblock.mahakala.is/hosts" ##10,528K  330,332 hosts
 alias elog='logger -t ADBLOCK -s'
 LocalHost="$CIFS/HOST-S\$i"
 
-[ -d $CIFS ] && TMP="$CIFS/tmp" || TMP="/tmp/tmp"
-[ -d $CIFS ] && GEN="$CIFS/gen" || GEN="/tmp/gen"
+[ -d "$CIFS" ] && TMP="$CIFS/tmp" || TMP="/tmp/tmp"
+[ -d "$CIFS" ] && GEN="$CIFS/gen" || GEN="/tmp/gen"
+[ -d "$CIFS" ] && GENHOST="$CIFS/genHost" || GEN="/tmp/genHost"
 
-# Check if another instance
+# Check if already running
 pidfile=/var/run/adblock.pid
 kill -0 $(cat $pidfile 2>/dev/null) &>/dev/null && {
 	elog "Another instance found ($pidfile, $(cat $pidfile)), exiting!"
@@ -48,7 +55,7 @@ stop() {
 
 pexit() {
 	elog "Exiting"
-	rm $TMP $GEN $pidfile &>/dev/null
+	rm "$TMP" "$GEN" "$GENHOST" $pidfile &>/dev/null
 	exit $@
 }
 
@@ -62,34 +69,60 @@ esac
 
 # Remove whitelisted site from the file
 Whitelist() {
+# Work only once dnsmasq formated.
+# Remove only listed domain.  eg; whitelist "facebook" will remove nothing.
+# eg; whitelist "facebook.com"  will remove ad.facebook.com and facebook.com but will continue to block, facebook.ca, fakefacebook.com, etc..
 for w in $WHITELIST; do
-sed -i -e "/\.$w/d" -e "/\/$w/d" $TMP
+sed -i -e "/\.$w/d" -e "/\/$w/d" "$TMP"
+done
+}
+WhitelistAll(){ 
+# used with KeepAsHost
+# remove every line containing whitelist.  eg; whitelist "facebook" will remove  ad.facebook.com, facebook.com, facebook.ca, fakefacebook.com, etc..
+for w in $WHITELIST; do
+sed -i -e "/$w/d" "$TMP"
 done
 }
 
-# Format the file for dnsmasq
+FormatDnsmasq() {
+sed -i -e '/^www[0-9]\./ s/^www[0-9]\.//
+/^www\./ s/^www\.//
+s|$|/'$REDIRECTIP'|
+s|^|address=/|' "$TMP"
+}
+FormatHost() {
+sed -i -e "s|^|$REDIRECTIP\\t|" "$TMP"
+}
+
+# Format the file
 Generate() {
-sed -i -e '
-/%/d
+sed -i -e '/%/d
 s/[[:cntrl:][:blank:]]//g
 s/^[ \t]*//;s/[ \t]*$//
 s/[[:space:]]*\[.*$//
 s/[[:space:]]*\].*$//
-/[[:space:]]*#.*$/ s/[[:space:]]*#.*$//  
-/^$/d
+/[[:space:]]*#.*$/ s/[[:space:]]*#.*$//
 /127\.0\.0\.1/ s/^127\.0\.0\.1[ \t]*//
-/^www[0-9]\./ s/^www[0-9]\.//   
-/^www\./ s/^www\.//
-s|$|/'$REDIRECTIP'|
-s|^|address=/|' $TMP
-Whitelist
-#cat $TMP >> $GEN
-#cat $GEN | sort -u > $TMP || elog "ERROR ; sort failed"
-#mv -f $TMP $GEN
-[ -f $GEN ] || echo "" > $GEN
-if [ -d $CIFS ]; then sort -o $TMP -T $CIFS $TMP && sort -m -u -o $GEN -T $CIFS $TMP $GEN || elog "ERROR ; sort failed"
-else sort -o $TMP $TMP && sort -m -u -o $GEN $TMP $GEN || elog "ERROR ; sort failed"; fi
-rm $TMP &>/dev/null
+/^$/d
+/[a-zA-Z0-9]\{5\}$/d' "$TMP"
+# /localhost$/d & # /localdomain$/d & # /broadcasthost$/d
+# replaced by /[a-zA-Z0-9]\{5\}$/d # That remove anything that have more than five Char at the end (dot not include)
+[ -n "$KeepAsHost" ] && [[ $KeepAsHost =~ $1 ]] && {
+	FormatHost # For host file
+	Result="$GENHOST"
+	WhitelistAll
+		} || {
+			FormatDnsmasq # For dnsmasq config
+			Result="$GEN"
+			Whitelist
+			}
+#cat $TMP >> $Result
+#cat $Result | sort -u > $TMP || elog "ERROR ; sort failed"
+#mv -f $TMP $Result
+[ ! -f "$Result" ] && echo "" > "$Result"
+if [ -d "$CIFS" ]; then sort -o "$TMP" -T "$CIFS" "$TMP" && sort -m -u -o "$Result" -T "$CIFS" "$TMP" "$Result" || elog "ERROR ; sort failed"
+else sort -o "$TMP" "$TMP" && sort -m -u -o "$Result" "$TMP" "$Result" || elog "ERROR ; sort failed"; fi
+rm "$TMP" &>/dev/null
 wait
 }
 
@@ -102,15 +135,15 @@ DL() {
 	for i in $DLList; do
 		eval url="\$S$i"
 		eval LocalFile="$LocalHost"
-		if wget $url -U "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)" -O - > $TMP ; then
+		if wget $url -O - > $TMP || wget $url -U "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)" -O - > $TMP ; then
 		elog "S$i downloaded $url"
-		[ -d $CIFS ] && cp -f $TMP $LocalFile
-		Generate
+		[ -d "$CIFS" ] && cp -f "$TMP" "$LocalFile"
+		Generate $i
 		else
-		[ -f $LocalFile ] && {
+		[ -f "$LocalFile" ] && {
 		elog "S$i update failed: load $LocalFile"
-		cp -f $LocalFile $TMP
-		Generate
+		cp -f "$LocalFile" "$TMP"
+		Generate $i
 		} || elog "S$i failed $url"
 		fi
 	done
@@ -119,10 +152,10 @@ DL() {
 [ -n "$GenOnly" ] && {
 	for i in $GenOnly; do
 		eval LocalFile="$LocalHost"
-		[ -f $LocalFile ] && {
+		[ -f "$LocalFile" ] && {
 			elog "Loading $LocalFile"
-			cp -f $LocalFile $TMP
-			Generate
+			cp -f "$LocalFile" "$TMP"
+			Generate $i
 		}
 	done
 }
@@ -137,8 +170,8 @@ local LAST=S$i.last
 eval LocalFile="$LocalHost"
 eval url="\$S$i"
 unset LASTF time
-[ -f "/tmp/$LAST" ] && LASTF=/tmp/$LAST
-[ -f "$CIFS/$LAST" ] && LASTF=$CIFS/$LAST
+[ -f "/tmp/$LAST" ] && LASTF="/tmp/$LAST"
+[ -f "$CIFS/$LAST" ] && LASTF="$CIFS/$LAST"
 P1=$(echo $url| sed 's|^http[s]*://[^/]*\(/.*\)$|\1|')
 H1=$(echo $url| sed 's|^http[s]*://\([^/]*\)/.*$|\1|')
 time=$(echo -e "HEAD $P1 HTTP/1.1\r\nHost: $H1\r\nConnection: close\r\n"|
@@ -154,8 +187,8 @@ nc -w 5 $H1 80|grep -i Last-Modified:|tr -d "\r")
 			[ -f "$LocalFile" ] && UpToDateLocal="$UpToDateLocal $i" || UpToDate="$UpToDate $i"
 		}
 	# Write modified time to RAM and in CIFS
-	[ -d $CIFS ] && echo "$time" >$CIFS/$LAST
-	echo "$time" >/tmp/$LAST
+	[ -d "$CIFS" ] && echo "$time" >"$CIFS/$LAST"
+	echo "$time" >"/tmp/$LAST"
 	} || {
 	# No "last modified" then download
 	# LocalOnly host file are always up to date
@@ -175,26 +208,34 @@ wait
 
 
 eval START=$(date +%s)
-rm $GEN &>/dev/null
+rm "$GEN" &>/dev/null
 cru d ADBTmpCheck &>/dev/null
-[ "$CIFSRequire" == "N" -o -d $CIFS ] && {
+[ "$CIFSRequire" == "N" -o -d "$CIFS" ] && {
 	CheckUpdate
 	DL
-	[ -f $GEN ] && {
-		SIZE=$(du -sh $GEN | awk '{ print $1 }')
+	[ -f "$GEN" -o -f "$GENHOST" ] && {
 		service dnsmasq stop &>/dev/null
 		killall -9 dnsmasq &>/dev/null
 		wait
 		# Add the original config file
-		cat /etc/dnsmasq.conf >> $GEN
+		cat /etc/dnsmasq.conf >> "$GEN"
+		# add the additionnal host
+		[ -f "$GENHOST" ] && {
+			echo "addn-hosts=$GENHOST" >>"$GEN"
+			# This adding stuff to the host for it to be valid on a pc if somebody desire to copy it an use it
+			sed -i '1s/^/#<localhost>\n127.0.0.1\tlocalhost\n127.0.0.1\tlocalhost.localdomain\n255.255.255.255\tbroadcasthost\n::1\tlocalhost\n127.0.0.1\tlocal\nfe80::1%lo0\tlocalhost\n#<\/localhost>\n/' "$GENHOST"
+			}
+		SIZE=$(du -sh $GEN | awk '{ print $1 }')
 		# Start blocking
-		dnsmasq --conf-file=$GEN && {
-			eval BlockCount=$(grep -c 'address=/' $GEN)
+		dnsmasq --conf-file="$GEN" && {
+			eval BlockCount=$(grep -c 'address=/' "$GEN")
+			[ -f "$GENHOST" ] && BlockCount=$(( $BlockCount+$(awk 'END { print NR }' "$GENHOST")))
 			eval END=$(date +%s)
 			eval DIFF=$(($END-$START))
 			# EXTRA 
-			[ -d $CIFS ] && echo ADBLOCK blocked $BlockCount unique host in $DIFF seconds > $CIFS/counts.txt
-			[ -d $CIFS ] && cp -f $GEN $CIFS/dnsmask.conf
+			[ -d "$CIFS" ] && echo ADBLOCK blocked $BlockCount unique host in $DIFF seconds > "$CIFS/counts.txt"
+			[ -d "$CIFS" -a -f "$GEN" ] && cp -f "$GEN" "$CIFS/dnsmask.conf"
+			[ -d "$CIFS" -a -f "$GENHOST" ] && cp -f "$GENHOST" "$CIFS/addHost.conf"
 			elog "Blocked $BlockCount unique host in $DIFF seconds"
 			} || {
 			elog "ERROR ; dnsmasq config ($SIZE) failed"
